b5456beefc7570a4d75626fe7ce8b9af7110e176
srsohan284 <srsohan284.com>
2022-07-29 01:11:46 +0600

    second commit

--- src/Pages/Home/Products/Jainamaj.js
+++ src/Pages/Home/Products/Jainamaj.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import useProducts from './../../../Hooks/useProducts';
+import Jainamaj1 from './Jainamaj1';
+
 
 const Jainamaj = () => {
     const [products] = useProducts([]);
     return (
         <div>
-          
+            {
+products.map(jainamaj=><Jainamaj1 jainamaj={jainamaj}></Jainamaj1>)
+            }
         </div>
-    );
+    )
+  
+  
 };
 
 export default Jainamaj;
