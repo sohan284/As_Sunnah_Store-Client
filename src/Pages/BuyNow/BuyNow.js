35492f0a932919bc5ec1f647b2f010f4d060cdd4
srsohan284 <srsohan284.com>
2022-08-08 15:30:02 +0600

    link

--- src/Pages/BuyNow/BuyNow.js
+++ src/Pages/BuyNow/BuyNow.js
@@ -8,7 +8,7 @@ const BuyNow = () => {
     const [product, setProduct] = useState({});
 
     useEffect(() => {
-        const url = `http://localhost:5000/product/${productId}`;
+        const url = `https://as-sunnah.herokuapp.com/product/${productId}`;
         fetch(url)
             .then(res => res.json())
             .then(data => setProduct(data))
